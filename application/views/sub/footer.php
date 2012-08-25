6f10c8adc032666c28e79f6fa53abb0cc8c9d838
Philipp Tessenow <philipp@tessenow.org>
2012-08-25 21:43:28 +0200

    slimlined html and corrected image paths when base url is not the root path

--- application/views/sub/footer.php
+++ application/views/sub/footer.php
@@ -1,13 +1,10 @@
 		<footer id="footer" role="contentinfo">
-			<p><a href="http://jfoucher.com" title="PHP web developer">Jonathan Foucher</a> made this, inspired by <a href="http://placekitten.com">placekitten</a> -
-                <a href="/about" title="About flickholdr.com">about</a> - <a href="/donate" title="If you like it click here!">donate</a></p>
+			<p><a href="http://jfoucher.com" title="PHP web developer">Jonathan Foucher</a> made this, inspired by <a href="http://placekitten.com">placekitten</a></p>
 		</footer><!-- /#footer -->
 
 	</div><!-- /#container -->
 <a href="https://github.com/jfoucher/flickholdr">
     <img style="position: fixed; top: 0; right: 0; border: 0;" src="http://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub" />
 </a>
-<script type="text/javascript" src="https://apis.google.com/js/plusone.js"></script>
-<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
 </body>
 </html>
--- application/views/sub/image_grid.php
+++ application/views/sub/image_grid.php
@@ -1,28 +1,10 @@
-			<div id="image-grid">
-				<img id="image-1" src="/250/300/forest,tree" alt="city" width="250" height="300">
-				<?php /*<script type="text/javascript"><!--
-                google_ad_client = "ca-pub-4626290100952665";
-                /* flickholdr200x200 *//*
-                google_ad_slot = "6860527238";
-                google_ad_width = 200;
-                google_ad_height = 200;
-                //-->
-                </script>
-                <script type="text/javascript"
-                src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
-                </script>
-        */?>
-                <?php
-                if(rand(1,10)>6){
-                ?>
-                    <a href="http://6px.eu" title="Web development by Six Pixels"><img id="image-2" src="/assets/img/6px.jpg" width="200" height="200" alt="Six Pixels Logo"></a>
-                <?php }else{?>
-                <img id="image-2" src="/200/200/forest,sunset" width="200" height="200" alt="bridge,sunrise">
-                <?php } ?>
-				<img id="image-3" src="/100/90/forest,sunrise" width="100" height="90" alt="city,sunrise">
-				<img id="image-4" src="/90/90/forest,sunset" width="90" height="90" alt="city,sunset">
-				<img id="image-5" src="/460/200/forest,mountain" width="460" height="200" alt="city,sunset">
-				<img id="image-6" src="/230/400/forest,nature" width="230" height="400" alt="sunset,nature">
-				<img id="image-7" src="/220/130/forest,mountain" width="220" height="130" alt="sunset,mountain">
-                <img id="image-8" src="/220/260/forest,mountain" width="220" height="260" alt="sunset,mountain">
-			</div>
+<div id="image-grid">
+	<img id="image-1" src="/rndimg/250/300/forest,tree" alt="city" width="250" height="300">
+	<img id="image-2" src="/rndimg/200/200/forest,sunset" width="200" height="200" alt="bridge,sunrise">
+	<img id="image-3" src="/rndimg/100/90/forest,sunrise" width="100" height="90" alt="city,sunrise">
+	<img id="image-4" src="/rndimg/90/90/forest,sunset" width="90" height="90" alt="city,sunset">
+	<img id="image-5" src="/rndimg/460/200/forest,mountain" width="460" height="200" alt="city,sunset">
+	<img id="image-6" src="/rndimg/230/400/forest,nature" width="230" height="400" alt="sunset,nature">
+	<img id="image-7" src="/rndimg/220/130/forest,mountain" width="220" height="130" alt="sunset,mountain">
+	<img id="image-8" src="/rndimg/220/260/forest,mountain" width="220" height="260" alt="sunset,mountain">
+</div>
